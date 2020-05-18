b6c0f4f3ccb4fae1741bba79cb13a17aef45112d
Oguzhan INAN <oguzhan3488@gmail.com>
2020-05-19 02:42:17 +0300

    Refactor xcomponents

--- src/renderer/App.vue
+++ src/renderer/App.vue
@@ -1,38 +1,6 @@
 <template>
   <div id="app">
     <router-view></router-view>
-    <Icons />
+    <TheIcons />
   </div>
 </template>
-
-<script>
-import Icons from './Icons'
-
-export default {
-  components: {
-    Icons
-  }
-}
-</script>
-
-<style>
-@import url('https://fonts.googleapis.com/css?family=Source+Sans+Pro');
-
-* {
-  box-sizing: border-box;
-  margin: 0;
-  padding: 0;
-  outline: 0;
-}
-
-body {
-  font-family: 'Source Sans Pro', sans-serif;
-  overflow: hidden;
-}
-
-#app {
-  width: 100vw;
-  height: 100vh;
-  background-color: black;
-}
-</style>
