6b29c910b301c730e948309c455f42814e77d104
Erhan Yakut <yakuter@gmail.com>
2020-06-14 02:40:02 +0300

    update secure key

--- src/renderer/store/index.js
+++ src/renderer/store/index.js
@@ -30,7 +30,7 @@ export default new Vuex.Store({
       state.master_hash   = this._vm.$helpers.pbkdf2Encrypt(master_password, data.secret)
       state.access_token  = data.access_token
       state.refresh_token = data.refresh_token
-      state.secure_key    = data.secure_key
+      state.secure_key    = data.secure_key.substr(0, 32)
       state.user          = data
 
       localStorage.access_token  = data.access_token
