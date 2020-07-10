d919f47078bf2aee71241b7066938518e3207768
umutbozdag <umutbozdag@outlook.com.tr>
2020-07-10 23:06:40 +0300

    #15 Get company logos from clearbit and show it to users (#17)
    Closes #15 
    * create company logo component
    * use logo component in login and server items
    * remove console.log
    * add regex to extract domain from url

---
+++ src/renderer/components/CompanyLogoItem.vue
@@ -0,0 +1,46 @@
+<template>
+  <img v-if="url && logoIsAvailable" @error="onLogoError" :src="companyLogo" />
+  <div v-else></div>
+</template>
+
+<script>
+export default {
+  name: 'CompanyLogoItem',
+
+  props: {
+    url: {
+      type: String,
+      default: () => ('')
+    }
+  },
+
+  data() {
+    return {
+      logoIsAvailable: true
+    }
+  },
+
+  computed: {
+    companyLogo() {
+      return `http://logo.clearbit.com/${this.domainFromURL}`
+    },
+
+    domainFromURL() {
+      // Regex is from: https://stackoverflow.com/a/33651369/10991790
+      const matches = this.url.match(/^(?:https?:)?(?:\/\/)?([^\/\?]+)/i)
+      return matches && matches[1]
+    }
+  },
+
+  methods: {
+    onLogoError(e) {
+      this.logoIsAvailable = false
+    }
+  }
+
+}
+</script>
+
+<style scoped lang="scss">
+
+</style>
