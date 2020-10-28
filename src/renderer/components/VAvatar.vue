aadc0f9c0f84f57e37948c890eb413dd037bc1a0
Faruk Oruç <ooruc471@yandex.com>
2020-10-28 19:25:01 +0300

    Fancy border pro account (#34)
    * Fancy avatar

---
+++ src/renderer/components/VAvatar.vue
@@ -0,0 +1,56 @@
+<template>
+  <div :class="[{ fancy: pro }, 'avatar']">
+    <div :class="[{ fancy: pro }, 'avatar-content']" v-text="firstLettersOfName" />
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'VAvatar',
+  props: {
+    name: String,
+    pro: Boolean
+  },
+  computed: {
+    firstLettersOfName() {
+      const [firstName, lastName] = this.name.split(' ')
+      return `${firstName[0]}${(lastName || ' ')[0]}`
+    }
+  }
+}
+</script>
+
+<style lang="scss">
+.avatar {
+  width: 40px;
+  height: 40px;
+  min-width: 40px;
+  min-height: 40px;
+  border-radius: 50%;
+
+  &:global(.fancy) {
+    background-origin: border-box;
+    background-clip: content-box, border-box;
+    border: double 2px transparent;
+
+    background-image: linear-gradient(black, black),
+      linear-gradient(45deg, $color-secondary, $color-primary);
+  }
+
+  &-content {
+    display: flex;
+    width: 100%;
+    height: 100%;
+    justify-content: center;
+    align-items: center;
+    border-radius: 50%;
+    background-color: $color-primary;
+    font-weight: 700;
+    color: #fff;
+
+    &:global(.fancy) {
+      background: black;
+    }
+  }
+}
+</style>
